73929a775a31b704194d56164dbffb9799f0f8b3
John <johnathan.bizzano@gmail.com>
2022-11-30 09:56:13 -0500

    Newest Version

--- Final.jl
+++ Final.jl
@@ -1,126 +1,160 @@
 using Plots
-using LinearAlgebra
+using FastBroadcast
+gr()
 
 const FP = Float64
-const FVec = NTuple{3, FP}
-const FZeroFvec = ntuple(x->FP(0), 3)
-const G = 3
+const G = 6.68E-20
 
 struct Body
-    m::FP
-    r::FVec
-    v::FVec
+    μ::FP
+    pv::Array{FP, 2}
+    tpva::Array{FP, 2}
+    tlk::Array{FP, 2}
+    tnk::Array{FP, 2}
 
-    Body(m, r, v) = new(m, FVec(r), FVec(v))
+    function Body(m, r, v)
+        pv = zeros(FP, 2, 3)
+        pv[1, :] = r
+        pv[2, :] = v
+        return new(G * m, pv, zeros(FP, 3, 3), zeros(FP, 2, 3), zeros(FP, 2, 3))
+    end
 end
 
 struct Universe{N}
     Bodies::Vector{Body}
-
     Universe(bodies...) = new{length(bodies)}(collect(bodies))
-
     Base.length(::Universe{N}) where N = N
 end
 
-function rk4_nbody_integrator(u::Universe{N}, iteration_callback::Function, T) where N
+function rk4(t, δ, pv, tpv, lk, nk, va)
+    tpv .= pv
+    lk .= δ .* va(t, pv)
+    nk .= lk
+
+    tpv .= pv .+ lk ./ 2
+    lk .= δ .* va(t + δ/2, tpv)
+    nk .+= 2 .* lk
+
+    tpv .= pv .+ lk ./ 2
+    lk .= δ .* va(t + δ/2, tpv)
+    nk .+= 2 .* lk
+
+    tpv .= pv .+ lk
+    lk .= δ .* va(t + δ, tpv)
+    nk .+= lk
+
+    tpv .= pv .+ nk / 6
+end
+
+polynomial_integrator(t, δ, pv, tpv, lk, nk, va) = tpv .= pv .+ δ .* va(t, pv)
+
+function nbody(u::Universe{N}, integrator::F, iteration_callback::F2, T) where {N, F, F2}
     δ = step(T)
     bds = u.Bodies
-    tpos = Array{FVec}(undef, N, 2)
-
-    function single_body2body_rk4(t, i1, i2)
-        GravitConst = G * bds[i2].m
-        z = (t, r, v) -> v
-        zp = function (t, r, v)
-                d = norm(r)
-                (d == 0) && return ZFVec
-                return GravitConst .* r ./ d ^ 3
-             end
-
-        r = bds[i2].r .- bds[i1].r
-        v = bds[i1].v
-        
-        k1 = δ .* zp(t, r, v)
-        l1 = δ .* z(t, r, v)
-
-        tv = v .+ k1 ./ 2
-        tr = r .+ l1 ./ 2
-        nt = t + δ / 2
-
-        k2 = δ .* zp(nt, tr, tv)
-        l2 = δ .* z(nt, tr, tv)
-
-        tr = r .+ l2 ./ 2
-        tv = v .+ k2 ./ 2
-
-        k3 = δ .* zp(nt, tr, tv)
-        l3 = δ .* z(nt, tr, tv)
-
-        tr = r .+ l3
-        tv = v .+ k3
-        nt = t + δ
-
-        k4 = δ .* zp(nt, tr, tv)
-        l4 = δ .* z(nt, tr, tv)
-        
-        dv = (k1 .+ 2 .* k2 .+ 2 .* k3 .+ k4) ./ 6
-        dr = (l1 .+ 2 .* l2 .+ 2 .* l3 .+ l4) ./ 6
-
-        return (dr, dv)
-    end
-
-    function single_body_rk4(t_0, i1)
-        netr = bds[i1].r
-        netv = bds[i1].v
-        for i2 in 1:N
-            (i2 == i1) && continue
-            dr, dv = single_body2body_rk4(t_0, i1, i2)
-            netr = netr .+ dr
-            netv = netv .+ dv
-        end
-        return (netr, netv)
-    end
 
     i = 1
     for t in T
         Threads.@threads for b in 1:N
-            tpos[b, :] .= single_body_rk4(t, b)
+            bod = u.Bodies[b]
+            tpva = bod.tpva
+
+            r = @view(tpva[1, :])
+            tpv = @view(tpva[1:2, :])
+            va = @view(tpva[2:3, :])
+            v = @view(tpva[2, :])
+            a = @view(tpva[3, :])
+
+            integrator(t, δ, bod.pv, tpv, bod.tlk, bod.tnk,   
+                function single_nbody_velacc(t, pv)
+                    a .= 0
+                    for b2 in 1:N
+                        q = bds[b2]
+                        r .= @view(q.pv[1, :]) .- @view(pv[1, :])           #Solve for the r vector representing the vector from p -> q 
+                        d = sqrt(sum(r -> r ^ 2, r))                        #Solve for the distance of r
+                        (d == 0) && continue                                #Probaly the same object so have it stick or do nothing
+                        a .+= r .* q.μ / d ^ 3
+                    end
+                    va[1, :] .= pv[2, :]
+                    return va
+                end)
         end
 
         for b in 1:N
-            bds[b] = Body(bds[b].m, tpos[b, 1], tpos[b, 2])
+            bod = u.Bodies[b]
+            bod.pv .= @view(bod.tpva[1:2, :])
         end
-
         iteration_callback(t, i)
         i += 1
     end
-
 end
 
-function simulate(T, earth, bodies...)
-    u = Universe(earth, bodies...)
-    N = length(bodies) + 1
-    a = Animation()
-    plt = plot3d(N, xlim = (-500, 500), ylim = (-500, 500), zlim = (-500, 500), legend = false)
-    pts = Array{FVec, 1}(undef, N)
-    pts[1] = FZeroFvec
+function simulate(T, integrator, bodies...; plotevery = 50)
+    u = Universe(bodies...)
+    N = length(bodies)
+    pts = [view(b.pv, 1, 1:3) for b in u.Bodies]
+    com = zeros(FP, 3)
+    fei = 1
+    points = Array{NTuple{3, FP}, 2}(undef, Int(floor(length(T) / plotevery)), N + 1)
 
-    rk4_nbody_integrator(u, 
-        function(t, i)
-            earthb = u.Bodies[1].r
+    function CapturePoints()
+        for i in 1:length(pts)
+            r = pts[i]
+            points[fei, i] = (r[1], r[2], r[3])
+        end
+        points[fei, end] = (com[1], com[2], com[3])
+    end
+
+    function CenterOfMass()
+        com .= 0
+        m = 0.0
+        for i in 1:N
+            com .+= u.Bodies[i].μ * @view(u.Bodies[i].pv[1, :])
+            m += u.Bodies[i].μ
+        end
+        com ./= m
+        return com
+    end
+
+    function BuildPlot()
+        println("Building Plot...")
+        plt = plot3d(N + 1, 
+            xlabel="X [km]", ylabel="Y [km]", zlabel = "Z [km]", title = "$N Body Space Curve", 
+            xlims=(-1E5, 1E5), ylims=(-1E5, 1E5), zlims=(-1E5, 1E5),
+            legend = false)
+        a = Animation()
+        trajectories = [Vector{FP}(undef, fei * (N + 1)) for i in 1:3]
         
-            for n in 2:N
-                #Center To Earth
-                pts[n - 1] = u.Bodies[n].r
+        r = 1
+        for b in 1:(N+1)
+            for i in 1:3
+                trajectories[i][r] = points[r, b][i]
             end
-            
-            frame(a, scatter(plt, pts, color = :jet))
-            println("Finished Iteration $i")
+            r += 1
+        end
+
+        for r in eachrow(points)
+            scplt = scatter(plt, r, markersize = 3, color=:red)
+            scatter!(trajectories, markersize = 1, color=:gray)
+            frame(a, scplt)
+        end
+        return gif(a, fps=10)
+    end
+    
+    nbody(u, integrator,
+        function(t, i)
+            if i % plotevery == 0
+                CenterOfMass()
+                CapturePoints()
+                println("Finished Iteration $i")
+                fei += 1
+            end
         end, T)
 
-    return gif(a)
+    println("Universe Calculations completed")   
+    return BuildPlot()
 end
 
-
-earth = Body(20, [0, 0, 0], [0, 0, 0])
-moon = Body(90, [50, 30, 50], [0, 0, 0])
-simulate(0:.1:200, earth, moon)
+earth = Body(1E26, [0, 0, 0], [15, 25, 35])
+moon = Body(1E25, [3000, 2000, 0], [-5, 10, -10])
+#moon2 = Body(1E-5, [0, -100, 0], [0, 0, -sqrt(G)])
+simulate(0.0:.01:3600.0, polynomial_integrator, earth, moon; plotevery=1000)
--- RK4.jl
+++ RK4.jl
@@ -7,13 +7,12 @@ function rk_4(f::Function, initial, t_f, δ = 1)
     iter = abs(Int(round((t_f - t) / δ)))
 
     for i in 1:iter
-        k1 = f(t, x)
-        nt = t + δ/2
-        k2 = f(nt, x + δ * k1/2)
-        k3 = f(nt, x + δ * k2/2)
-        k4 = f(nt, x + δ * k3)
+        k1 = δ * f(t, x)
+        k2 = δ * f(t + δ/2, x + k1/2)
+        k3 = δ * f(t + δ/2, x + k2/2)
+        k4 = δ * f(t + δ, x + k3)
         
-        x += δ * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        x += (k1 + 2 * k2 + 2 * k3 + k4) / 6
         t = nt
     end
 
