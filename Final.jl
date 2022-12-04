753af153c7ad140bd36ff0dc9a6d951a88f39a6c
John <johnathan.bizzano@gmail.com>
2022-12-04 03:41:46 -0500

    Version 6

--- Final.jl
+++ Final.jl
@@ -1,24 +1,24 @@
-using Plots
 using ProgressMeter
-using SatelliteToolbox
 using GeometryBasics
 using GLMakie
 using FileIO
-gr()
+using Printf
 
 const FP = Float64
 const G = 6.67259E-20
+const BodySizeScale = 1E3
 
 struct Body
     μ::FP
     pv::Array{FP, 2}
     tpv::Array{FP, 2}
+    planet_size::Int32
 
-    function Body(m, r, v)
+    function Body(m, r, v, s)
         pv = zeros(FP, 2, 3)
         pv[1, :] = r
         pv[2, :] = v
-        return new(G * m, pv, zeros(FP, 2, 3))
+        return new(G * m, pv, zeros(FP, 2, 3), s * BodySizeScale)
     end
 end
 
@@ -28,6 +28,7 @@ struct Universe{N}
     Base.length(::Universe{N}) where N = N
 end
 
+#Integrators
 function rk4_integrator(t, δ, pv, va)
     k1 = δ .* va(t, pv)               
     k2 = δ .* va(t + δ/2, pv .+ k1 ./ 2)
@@ -35,8 +36,10 @@ function rk4_integrator(t, δ, pv, va)
     k4 = δ .* va(t + δ, pv .+ k3)
     return pv .+ (k1 + 2 * k2 + 2 * k3 + k4) / 6
 end
-
 polynomial_integrator(t, δ, pv, va) = pv .+ δ .* va(t, pv)
+
+rows(x) = size(x, 1)
+cols(x) = size(x, 2)
 
 function nbody(u::Universe{N}, integrator::F, iteration_callback::F2, T) where {N, F, F2}
     δ = step(T)
@@ -48,20 +51,20 @@ function nbody(u::Universe{N}, integrator::F, iteration_callback::F2, T) where {
             bod = u.Bodies[b]
 
             bod.tpv .= integrator(t, δ, bod.pv,   
-            function single_nbody_velacc(t, pv)
-                va = zeros(FP, 2, 3)
-                a = @view(va[2, :])
-                p = @view(pv[1, :])
-                va[1, :] .= pv[2, :]
-                for b2 in 1:N
-                    q = bds[b2]
-                    r = @view(q.pv[1, :]) - p                        #Solve for the r vector representing the vector from p -> q 
-                    d = sqrt(sum(r -> r ^ 2, r))                     #Solve for the distance of r
-                    (b == b2 || d == 0) && continue                  #Probaly the same object so have it stick or do nothing
-                    a .+= r * q.μ / d ^ 3
-                end
-                return va
-            end)
+                function single_nbody_velacc(t, pv)
+                    va = zeros(FP, 2, 3)
+                    a = @view(va[2, :])
+                    p = @view(pv[1, :])
+                    va[1, :] .= pv[2, :]
+                    for b2 in 1:N
+                        q = bds[b2]
+                        r = @view(q.pv[1, :]) - p           #Solve for the r vector representing the vector from p -> q 
+                        d = sqrt(sum(r -> r ^ 2, r))        #Solve for the distance of r
+                        (b == b2 || d == 0) && continue     #Probaly the same object so have it stick or do nothing
+                        a .+= r * q.μ / d ^ 3
+                    end
+                    return va
+                end)
         end
 
         for b in 1:N
@@ -73,74 +76,145 @@ function nbody(u::Universe{N}, integrator::F, iteration_callback::F2, T) where {
     end
 end
 
-function simulate(T, integrator, bodies...; plotevery = 50)
-    u = Universe(bodies...)
-    N = length(bodies)
-    pts = [view(b.pv, 1, 1:3) for b in u.Bodies]
-    com = zeros(FP, 3)
-    fei = 1
-    numPts = Int(floor(length(T) / plotevery))
-    points = Array{NTuple{3, FP}, 2}(undef, numPts, N + 1)
-
-    function CapturePoints()
-        for i in 1:length(pts)
-            r = pts[i]
-            points[fei, i] = (r[1], r[2], r[3])
-        end
-        points[fei, end] = (com[1], com[2], com[3])
-    end
-
-    function CenterOfMass()
-        com .= 0
-        m = 0.0
-        for i in 1:N
-            com .+= u.Bodies[i].μ * @view(u.Bodies[i].pv[1, :])
-            m += u.Bodies[i].μ
-        end
-        com ./= m
-        return com
-    end
-
-    function BuildPlot()
-        prog = Progress(numPts, desc="Building Plot", barglyphs=BarGlyphs("[=> ]"), barlen=50, color=:blue)
-        img = load("FAROOQM.jpg")    
-        limit = 2E6
-        limits = (-limit/2, limit/2, -limit/2, limit/2, -limit/2, limit/2)
-        a = Animation()
-
-        pei = 1
-        for r in eachrow(points)
-            f = Figure()
-            a = Axis(f[1,1], aspect = (1,1,1), viewmode = :fit, limits=limits)
-            earth = GeometryBasics.Sphere(r[1], 750)
-            mesh!(a, earth, color=img)
-            frame(a, scplt)
-            update!(prog, pei)
-            pei += 1
-        end
-
-        return gif(a, fps=5)
-    end
-    
-    prog = Progress(numPts, desc="Performing Universe Calculations", barglyphs=BarGlyphs("[=> ]"), barlen=50, color=:yellow)
-
-    nbody(u, integrator,
-        function(t, i)
-            if i % plotevery == 0
-                CenterOfMass()
-                CapturePoints()
-                update!(prog, fei)
-                fei += 1
-            end
-        end, T)
-
-    println("Universe Calculations completed")   
-    return BuildPlot()
+function Progressable(x::Function, ticks, desc, color)
+    prog = Progress(ticks, desc=desc, barglyphs=BarGlyphs("[=> ]"), barlen=50, color=color)
+    update!(prog, 1)
+    x(prog)
 end
 
+function CenterOfMass(bodies)
+    com = zeros(FP, 3)
+    m = 0.0
+    for b in bodies
+        com .+= b.μ * @view(b.pv[1, :])
+        m += b.μ
+    end
+    com ./= m
+    return com
+end
 
-earth = Body(1E26, [0, 0, 0], [15, 25, 35])
-moon = Body(1E25, [3000, 2000, 0], [-5, 10, -10])
-moon2 = Body(1E25, [3000, -2000, 0], [-5, -10, 10])
+BuildPlot(bodies, snap_shot, N) = Progressable(rows(snap_shot), "Building Plot", :blue) do prog
+    images = load.(["Earth.png", "FAROOQM.jpg", "Moon.jpg"])
+    set_theme!(backgroundcolor = :black)
+    fig = Figure()
+    ax = Axis3(fig[1, 1], aspect = (1, 1, 1), viewmode=:fit, limits = ((-2E4, 2E4), (-2E4, 2E4), (-2E4, 2E4)))
+    ax.titlecolor = :red
+    ax.xgridcolor = :green
+    ax.ygridcolor = :green
+    ax.zgridcolor = :green
+    ax.xticklabelcolor = :green
+    ax.yticklabelcolor = :green
+    ax.zticklabelcolor = :green
+    ax.title = "$N Body Solution"
 
-simulate(0.0:.5:3600, rk4_integrator, earth, moon, moon2; plotevery=100)
+    #Initialize Observables
+    initial_positions = [snap_shot[1, i] for i in 1:N]
+    body_translation_interfaces = [Observable(Point3f(0)) for i in 1:N]
+    trajectory_positions = [Observable(Point3f[]) for i in 1:(N+1)]
+
+    for i in 1:N
+        #Initialize Meshes
+        mesh!(ax, 
+            GeometryBasics.Sphere(Point3f(initial_positions[i]), bodies[i].planet_size), 
+            color = images[i > 3 ? 3 : i], 
+            transformation = Transformation(translation = body_translation_interfaces[i]))
+        
+        for b in 1:N; lines!(ax, trajectory_positions[b], color = :gray) end     #Initialize Trajectories
+        lines!(ax, trajectory_positions[end], color = :red)                      #Initialize COM Trajectory
+    end
+
+    return record(fig, "Trajectory.mp4", 1:rows(snap_shot); framerate = 10) do f
+        for w in 1:N
+            body_translation_interfaces[w][] = snap_shot[f, w] .- initial_positions[w]
+        end
+
+        for w in 1:(N + 1)
+            push!(trajectory_positions[w][], snap_shot[f, w])
+            notify(trajectory_positions[w])
+        end
+
+        autolimits!(ax)
+        update!(prog, f)
+    end
+end
+
+function CapturePoints(body_pos, snap_shot, com, shap_shot_idx)
+    for i in 1:length(body_pos)
+        r = body_pos[i]
+        snap_shot[shap_shot_idx, i] = (r[1], r[2], r[3])
+    end
+    snap_shot[shap_shot_idx, end] = (com[1], com[2], com[3])
+end
+
+InitWriteDataFile(N) = begin
+    io = open("data.dat", "w")
+    write(io, "t\t\t\t|")
+    for i in 1:N
+        write(io, "\t\t\t\tr$i\t\t\t\t|\t\t\t\tv$i\t\t\t\t|")
+    end
+    write(io, "\n")
+    return io
+end
+
+WriteDataFileEntry(bodies, t, io) = begin
+    numf(n) = string(@sprintf("%9.2f", n))
+    write_array(v) = join([numf(i) for i in v])
+    write(io, "$(numf(t))\t|")
+    for b in bodies
+        write(io, "\t$(write_array(@view(b.pv[1, :])))\t|\t$(write_array(@view(b.pv[2, :])))\t|")
+    end
+    write(io, "\n")  
+end
+
+function simulate(T, integrator, bodies...; plotevery = 50, writedata = false)
+    u = Universe(bodies...)
+    N = length(bodies)
+    body_pos = [view(b.pv, 1, 1:3) for b in u.Bodies]
+    shap_shot_idx = 1
+    frames = Int(floor(length(T) / plotevery))
+    snap_shot = Array{NTuple{3, FP}, 2}(undef, frames, N + 1)
+
+    println("Initializing $N body Universe")
+    println("T_0 = $(first(T)) seconds")
+    println("Step = $(step(T)) seconds")
+    println("T_F = $(last(T)) seconds")
+
+    if writedata
+        io = InitWriteDataFile(N)
+        WriteDataFileEntry(u.Bodies, 0, io)
+    end
+    
+    function print_state_vectors(isFirst)
+        name = isFirst ? "i" : "f"
+        for i in 1:N
+            isFirst && (println("m_$i = ", u.Bodies[i].μ / G))
+            println("R_{$name, $i} = ", u.Bodies[i].pv[1, :], " km")
+            println("V_{$name, $i} = ", u.Bodies[i].pv[2, :], " km")
+            println()
+        end
+    end
+
+    print_state_vectors(true)
+
+    Progressable(frames, "Performing Universe Calculations", :yellow) do prog
+        nbody(u, integrator,
+            function(t, i)
+                if i % plotevery == 0
+                    CapturePoints(body_pos, snap_shot, CenterOfMass(u.Bodies), shap_shot_idx)
+                    update!(prog, shap_shot_idx)
+                    writedata && WriteDataFileEntry(u.Bodies, t, io)
+                    shap_shot_idx += 1
+                end
+            end, T)
+    end
+
+    print_state_vectors(true)
+    writedata && (close(io))
+
+    return BuildPlot(u.Bodies, snap_shot, N)
+end
+
+earth = Body(1E26, [0, 0, 0], [15, 25, 35], 2)
+moon = Body(1E25, [3000, 2000, 0], [-5, 10, -10], 1)
+moon2 = Body(1E25, [3000, -2000, 0], [-5, -10, 10], 1)
+simulate(0.0:.1:3600, rk4_integrator, earth, moon, moon2; plotevery=100, writedata = true)
